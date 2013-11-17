0ee64fcb01c826b77532f9e2af4e0d55b8edef3b
Hongxu Liu <djshu.us@gmail.com>
2013-11-17 14:09:36 -0800

    Animated transition from users to places

--- www/js/controllers/place.js
+++ www/js/controllers/place.js
@@ -1,7 +1,6 @@
 angular.module('GetTogetherApp')
 .controller('PlaceCtrl', function($scope, MarkerService, MapService, PanService) {
   $scope.centerPlace = function(place) {
-    console.log(place);
     var map = MapService.map;
     if(place.position) {
       PanService.centerByPosition(place.position, map);

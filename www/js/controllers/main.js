d83bc662ee57ccd34e46d2de15d06ce01ee32be3
Hongxu Liu <djshu.us@gmail.com>
2013-11-17 10:35:19 -0800

    Changed places and users tabs to come down instead of up

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -5,26 +5,6 @@ angular.module('GetTogetherApp')
   //   e.preventDefault();
   // }, false);
 
-  $scope.toggleUsers = function() {
-    $scope.showPlaces = false;
-    $scope.showChat = false;
-    $scope.showUsers = !$scope.showUsers;
-  };
-
-  $scope.togglePlaces = function() {
-    $scope.showUsers = false;
-    $scope.showChat = false;
-    $scope.showPlaces = !$scope.showPlaces;
-  };
-
-  $scope.toggleChat = function() {
-    $scope.showUsers = false;
-    $scope.showPlaces = false;
-    $scope.showChat = !$scope.showChat;
-    ChatService.scrollToBottom();
-
-  };
-
   $scope.panel = {};
   $scope.input = {
     blur: function() {
---
+++ www/js/controllers/tabs.js
@@ -0,0 +1,26 @@
+angular.module('GetTogetherApp')
+.controller('MapCtrl', function($scope) {
+  $scope.toggleUsers = function() {
+    $scope.showPlaces = false;
+    $scope.showChat = false;
+    $scope.showUsers = !$scope.showUsers;
+  };
+
+  $scope.togglePlaces = function() {
+    $scope.showUsers = false;
+    $scope.showChat = false;
+    $scope.showPlaces = !$scope.showPlaces;
+  };
+
+  $scope.toggleChat = function() {
+    $scope.showUsers = false;
+    $scope.showPlaces = false;
+    $scope.showChat = !$scope.showChat;
+    ChatService.scrollToBottom();
+  };
+
+  $scope.hideTabs = function() {
+    $scope.showUsers = false;
+    $scope.showPlaces = false;
+  };
+});
